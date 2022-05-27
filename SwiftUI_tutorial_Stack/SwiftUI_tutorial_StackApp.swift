128a5d81ea1dedd13aaa5e922df902e9c716aabe
Jun Hyeong An <99971427+JunHyeongAn@users.noreply.github.com>
2022-05-27 16:02:53 +0900

    Initial Commit

---
+++ SwiftUI_tutorial_Stack/SwiftUI_tutorial_StackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_tutorial_StackApp.swift
+//  SwiftUI_tutorial_Stack
+//
+//  Created by 안준형 on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_tutorial_StackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
